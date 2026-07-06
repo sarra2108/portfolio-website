50e7f94a4b6c92dcb0bac6b42e5a457f38f7a07f
sarra2108 <ssass058@uottawa.ca>
2026-07-06 19:29:26 +0100

    new fix

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
-import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
-
-resolve:{
-  alias:{
-    "@": path.resolve(path.dirname(new URL(import.meta.url).pathname), './src'),
+  base: '/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
-}
-});
+})
